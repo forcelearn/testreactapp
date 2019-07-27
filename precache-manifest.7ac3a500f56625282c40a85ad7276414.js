22ea925b8407bbcd2c2a20282e9aa770cb60bcef
forcelearn <31430782+forcelearn@users.noreply.github.com>
2019-07-27 20:14:59 +0530

    Updates

---
+++ precache-manifest.7ac3a500f56625282c40a85ad7276414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2919088e09e269eeb23b4275859683",
+    "url": "/testreactapp/index.html"
+  },
+  {
+    "revision": "ddfea9b8846c3d40f306",
+    "url": "/testreactapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testreactapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ddfea9b8846c3d40f306",
+    "url": "/testreactapp/static/js/main.4e494456.chunk.js"
+  },
+  {
+    "revision": "11555c96fbdb746089a4",
+    "url": "/testreactapp/static/js/runtime~main.4feacf41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
